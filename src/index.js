3c90b324a0bc91e75496921ee35bf59b9f62a9d1
karan8509 <kj382542@gmail.com>
2025-05-16 01:57:48 +0330

    fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ToastContainer } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <ToastContainer />
+    <Toaster />
     <App />
   </>
 );
--- src/pages/quizBase.js
+++ src/pages/quizBase.js
@@ -2,49 +2,51 @@ import React, { useState, useEffect } from "react";
 import { quizQuestions } from "../api/quizQuestions.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "../style/quizBase.css";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {toast} from "react-hot-toast"
+
 
 const QuizBase = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [isQuizFinal, setIsQuizFinal] = useState(false);
+  const [isQuizFinal, setIsQuizFinal] = useState();
   const [selectedOption, setSelectedOption] = useState();
   const [time, setTime] = useState(30);
 
-
   useEffect(() => {
-            setTime(30);
-            const setTimerunning = setInterval(() => {
-                 setTime((prev) => prev - 1);
-            }, 1000);
-            const newTime = setTimeout(() => {
-                newCallOption()
-            }, 30000);
-            return () => {
-              clearInterval(setTimerunning)
-              clearTimeout(newTime)
-            }
-  }, [currentIndex])
+    setTime(30);
+    const setTimerunning = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+    const newTime = setTimeout(() => {
+      newCallOption();
+    }, 30000);
+    return () => {
+      clearInterval(setTimerunning);
+      clearTimeout(newTime);
+    };
+  }, [currentIndex]);
 
   const newCallOption = () => {
-     const newItem = currentIndex + 1
-     if(newItem <  quizQuestions.length){
-      setCurrentIndex(newItem)
-     }else{
-      setScore(true)
-     }
-  }
+    const newItem = currentIndex + 1;
+    if (newItem < quizQuestions.length) {
+      setCurrentIndex(newItem);
+     
+    } else {
+     setIsQuizFinal(true)
+    }
+  };
 
   const handleClick = (SelectionOption) => {
     if (SelectionOption === quizQuestions[currentIndex].correctAnswer) {
-      setScore(score + 1);
+      setScore(score + 1)
+    
+    
       toast.success("Correct Answer!", { autoClose: 1500 });
     } else if (SelectionOption !== "") {
       toast.error("Wrong Answer!", { autoClose: 1500 });
     }
 
-    newCallOption()
+    newCallOption();
   };
 
   return (
@@ -60,25 +62,31 @@ const QuizBase = () => {
             <h3>Quiz App</h3>
             <div className="col-sm-12">
               <h4>
-                Question {currentIndex + 1}: {quizQuestions[currentIndex].question} 
+                Question {currentIndex + 1}:{" "}
+                {quizQuestions[currentIndex].question}
               </h4>
               <p>Time: {time} s</p>
             </div>
           </div>
           <div className="list-group">
-            {quizQuestions[currentIndex].options.map((_element, _index) => (      // option print 
-              <label className="list-group-item" key={_index}>
-                <input
-                  className="form-check-input me-1"
-                  type="radio"
-                  id="option"
-                  checked={selectedOption === _element}
-                  name="option"
-                  onChange={() => handleClick(_element)}
-                />
-                {_element}
-              </label>
-            ))}
+            {quizQuestions[currentIndex].options.map(
+              (
+                _element,
+                _index // option print
+              ) => (
+                <label className="list-group-item" key={_index}>
+                  <input
+                    className="form-check-input me-1"
+                    type="radio"
+                    id="option"
+                    checked={selectedOption === _element}
+                    name="option"
+                    onChange={() => handleClick(_element)}
+                  />
+                  {_element}
+                </label>
+              )
+            )}
           </div>
         </center>
       )}
@@ -87,4 +95,3 @@ const QuizBase = () => {
 };
 
 export default QuizBase;
-
